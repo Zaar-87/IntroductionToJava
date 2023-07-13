4459a14a03749756c3c70cc53752d83c806b988e
Zaar <zaarinc@gmail.com>
2023-07-13 12:57:51 +0300

    6th seminar SEM added

--- Seminar6/SEM/Main.java
+++ Seminar6/SEM/Main.java
@@ -1,5 +1,37 @@
 package Seminar6.SEM;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+// Task 1. Создайте метод, формирующий массив из N случайных целых чисел.
+
 public class Main {
+
+
     
+    // ArrayList<Integer> myList = createArray(10, 10);
+    // {
+    //     // System.out.println(myList);
+    //     procent(myList);
+    // }
+
+    // static ArrayList<Integer> createArray(int n, int maxValue) {
+    //     ArrayList<Integer> list = new ArrayList<>();
+    //     Random random = new Random();
+    //     for (int i = 0; i < n; i++)
+    //         list.add(random.nextInt(maxValue));
+    //     return list;
+    // }
+
+    // static void procent(ArrayList<Integer> myArray){
+    //     double sizeArray = myArray.size();
+    //     Set set = new HashSet(myArray);
+    //     double sizeSet = set.size();
+    //     double res = sizeSet / sizeArray * 100;
+    //     System.out.println(myArray);
+    //     System.out.println(set);
+    //     System.out.printf("%s - процент от массива", res);
+    // }    
 }
